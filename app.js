245721ab87c137b081045ec83f173034a099e520
Philipp Baschke <philipp@baschke.com>
2016-06-13 17:07:31 +0200

    Create bin to start the api with certain host/port
    The bin makes it possible to start the api server with different hosts
    and ports.
    
    See: https://github.com/koajs/api-boilerplate/

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 import Koa from 'koa'
 
 const app = new Koa()
-const PORT = 3000
 
 app.use((ctx) => {
   ctx.body = 'Hello Koa'
 })
-
-app.listen(PORT)
 
 export default app
--- index.js
+++ index.js
@@ -2,4 +2,4 @@
  * https://github.com/koajs/koa/tree/2.0.0#babel-setup
  */
 require('babel-core/register')
-require('./app.js')
+module.exports = require('./app.js').default
