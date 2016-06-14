8004c1d32cb7dcc5b618b86c282a60c45af21c7e
Philipp Baschke <philipp@baschke.com>
2016-06-14 18:48:18 +0200

    Add middleware that uses mongoose to connect to DB

---
+++ middleware/mongoose.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+/**
+ * Middleware to open a mongoose connection and add it to the ctx
+ *
+ * @param {object} options The database options
+ * @return {function} A koa middleware function that adds db to ctx
+ */
+export default function mongooseMiddleware(options) {
+  let connectionString = 'mongodb://'
+
+  if (options.username && options.password) {
+    connectionString += `${options.username}:${options.password}@`
+  }
+  connectionString += `${options.host}:${options.port}/${options.db}`
+  mongoose.connect(connectionString)
+
+  return async (ctx, next) => {
+    ctx.db = mongoose.connection
+    await next()
+  }
+}
