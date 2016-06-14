773eae59731c292805b0866be75aabb77f92b598
Philipp Baschke <philipp@baschke.com>
2016-06-14 19:04:49 +0200

    Use mongoose middleware with settings from .env
    The database settings should come from the ENV. dotenv makes it possible
    to save them in a .env file for easier access.
    
    .env has to be gitignored to avoid commiting credentials accidentally.
    
    .env.example helps to understand the desired structure of .env.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+import dotenv from 'dotenv'
 import Koa from 'koa'
+import mongoose from './middleware/mongoose'
 
 const app = new Koa()
 
-app.use((ctx) => {
-  ctx.body = 'Hello Koa'
-})
+/**
+ * Load environment variables from .env
+ */
+dotenv.config()
+
+/**
+ * Database (with mongoose)
+ */
+app.use(mongoose({
+  'host': process.env.DB_HOST,
+  'port': process.env.DB_PORT,
+  'db': process.env.DB_NAME,
+  'username': process.env.DB_USERNAME,
+  'password': process.env.DB_PASSWORD
+}))
 
 export default app
