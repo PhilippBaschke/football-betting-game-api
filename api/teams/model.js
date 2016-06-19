ae286878ba7d176b055870a57d3fa20517f147db
Philipp Baschke <philipp@baschke.com>
2016-06-19 16:50:49 +0200

    Add name and crestUrl field to Team model
    The name e.g. France and a url to the crest (svg from wikipedia) are
    helpful to display the team (names match those of football-data.org).

--- api/teams/model.js
+++ api/teams/model.js
@@ -2,9 +2,22 @@ import mongoose from 'mongoose'
 
 // TODO: Use virtual fields to get the other fields (from the API)
 const Schema = new mongoose.Schema({
-  '_id': Number
+  '_id': Number,
+  'name': {
+    'type': String,
+    'required': true
+  },
+  'crestUrl': {
+    'type': String,
+    'required': true
+  }
 })
 
+const Team = mongoose.model('Team', Schema)
+
 export {
-  Schema
+  Schema,
+  Team
 }
+
+export default Team
