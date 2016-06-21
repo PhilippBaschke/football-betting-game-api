d769f1ebe98cfb627ad250fff561457d98dca50f
Philipp Baschke <philipp@baschke.com>
2016-06-21 16:24:23 +0200

    Add fixtures to soccerSeasons via middleware
    The fixtures should be added to soccerseasons when a find or a findOne
    operation is performed. This is done in a post middleware that adds a
    field 'fixtures' to the returned documents (disadvantage: clients need
    to call document.toObject() before they are serialized to include the
    fixtures as well).
    
    Fixtures are not added on .find() when the query is empty (all
    soccerSeasons should be shown) because the client probably wants just an
    overview und does not need all the information
    alreay (/api/soccerseasons/ will just return basic information without
    teams or fixtures).

--- api/soccerseasons/model.js
+++ api/soccerseasons/model.js
@@ -1,3 +1,4 @@
+import fp from 'lodash/fp'
 import mongoose from 'mongoose'
 
 const Schema = new mongoose.Schema({
@@ -37,6 +38,55 @@ Schema.pre(
             await this.constructor.footballData(`/soccerseasons/${this._id}`)
 
     this.set(apiData)
+    next()
+  }
+)
+
+const addFixtures = async (query, soccerSeason) => {
+  const fixturesApi = `/soccerseasons/${soccerSeason._id}/fixtures`
+  const apiData = await query.model.footballData(fixturesApi)
+
+  soccerSeason.set('fixtures', apiData.fixtures, {'strict': false})
+
+  return soccerSeason
+}
+
+Schema.post(
+  'find',
+
+  /**
+   * Add fixtures to all soccerSeasons
+   *
+   * @param {Array} soccerSeasons An array of mongoose Documents
+   * @param {function} next The next middleware
+   * @this mongoose.Query
+   * @return {undefined}
+   */
+  async function addFixturesOnFind(soccerSeasons, next) {
+    // Fixtures are not needed when all soccerseasons should be shown
+    if (fp.isEmpty(this.getQuery())) { return next() }
+
+    // Promise.all() to make sure that all addFixtures
+    // calls are finished before continuing
+    // See: https://github.com/tc39/ecmascript-asyncawait/issues/7
+    await Promise.all(fp.map(fp.partial(addFixtures, [this]), soccerSeasons))
+
+    return next()
+  }
+)
+
+Schema.post(
+  'findOne',
+
+  /**
+   * Add fixtures to the soccerSeason
+   * @param {mongoose.Document} soccerSeason A mongoose document
+   * @param {function} next The next middleware
+   * @this mongoose.Query
+   * @return {undefined}
+   */
+  async function addFixturesOnFindOne(soccerSeason, next) {
+    await addFixtures(this, soccerSeason)
     next()
   }
 )
